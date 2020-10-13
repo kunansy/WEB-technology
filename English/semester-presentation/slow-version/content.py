844e6165177457ee8f8bd3ece1fc6163feb0cb71
kunansy <kolobov.kirill@list.ru>
2020-10-13 16:38:43 +0300

    python version of code to semester presentation of English was completed

--- English/semester-presentation/slow-version/content.py
+++ English/semester-presentation/slow-version/content.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
+import math
+import time
+
+
+def measure_time(func):
+    def inner(*args, **kwargs):
+
+    return inner
+
+
+@measure_time
+def do_math(start: int = 0,
+            iters_count: int = 0) -> None:
+    pos = start
+    k_sq = 1_000_000
+    while pos < iters_count:
+        pos += 1
+        math.sqrt((pos - k_sq)**2)
 
 
 if __name__ == "__main__":
-    pass
+    start = time.time()
+    do_math(1, 10_000_000)
+    print(f"{time.time() - start:.1f}")
