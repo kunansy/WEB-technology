77c3da188e92fa5e52e267461203b584ed5d7a56
kunansy <kolobov.kirill@list.ru>
2020-10-13 16:37:17 +0300

    cython version of code to semester presentation of English was completed

--- English/semester-presentation/quick-version/content.py
+++ English/semester-presentation/quick-version/content.py
@@ -3,15 +3,6 @@ import time
 
 import calc
 
-
-def measure_time(func):
-    def inner(*args, **kwargs):
-        start = time.time()
-        func(*args, *kwargs)
-        print(f"{time.time() - start:.1f}")
-    return inner
-
-
 if __name__ == "__main__":
     start = time.time()
     calc.do_math(1, 10_000_000)
