234a3b50e810c258f302b7546065b267bc63e2dc
kunansy <kolobov.kirill@list.ru>
2020-10-31 23:41:33 +0300

    One task of lab6 was completed
    The task3 of programming basics of lab6 for Alexandra Smetanina was
    completed

--- Programming-basics/Labs-for-collegues/Alexandra-Smetanina/Lab6/task_3.py
+++ Programming-basics/Labs-for-collegues/Alexandra-Smetanina/Lab6/task_3.py
@@ -1,8 +1,45 @@
-#!/usr/bin/env python3
+import numpy as np
 
 
-def main():
-    pass
+def min_diagonal_sum(matrix):
+    _, size = matrix.shape
+    min_d, offset = sum(matrix.diagonal()), 0
+    for i in range(-size + 2, size - 1):
+        if sum(matrix.diagonal(i)) < min_d:
+            min_d = sum(matrix.diagonal(i))
+            offset = i
+    return offset, min_d
 
 
-main()
+def summ(matrix):
+    res = 0
+    for row in matrix:
+        if all(i >= 0 for i in row):
+            res += sum(row)
+    return res
+
+
+try:
+    with open('in.txt', 'r', encoding='utf-8') as f:
+        size = int(f.read())
+    assert size > 0
+except FileNotFoundError:
+    with open('out.txt', 'w', encoding='utf-8') as f:
+        f.write("Файл с входными данными не существует\n")
+    exit(-1)
+except Exception:
+    with open('out.txt', 'w', encoding='utf-8') as f:
+        f.write("Неверный ввод, ожидалось натуральное число\n")
+    exit(-1)
+
+with open('out.txt', 'w', encoding='utf-8') as f:
+    matrix = np.random.randint(low=-100, high=100, size=(size, size))
+
+    f.write(f"Матрица {size}x{size}:\n")
+    f.write(f"{matrix}\n")
+
+    f.write(f"Сумма строк с элементами >= 0: {summ(matrix)}\n")
+    offset, min_d = min_diagonal_sum(matrix)
+    f.write(f"Минимальная сумма элементов диагоналей, параллельных главной: {min_d}\n")
+    f.write(f"{matrix.diagonal(offset)}\n")
+
