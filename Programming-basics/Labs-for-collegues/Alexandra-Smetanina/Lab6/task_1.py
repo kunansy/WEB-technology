1fa56edb1e954d8a5c677751b9a54e13adca00fe
kunansy <kolobov.kirill@list.ru>
2020-11-03 23:55:46 +0300

    First task of 6th lab for collegue was completed
    Task 1 of lab 6 of programming basics for Alexandra Smetanina was
    completed

--- Programming-basics/Labs-for-collegues/Alexandra-Smetanina/Lab6/task_1.py
+++ Programming-basics/Labs-for-collegues/Alexandra-Smetanina/Lab6/task_1.py
@@ -1,8 +1,25 @@
-#!/usr/bin/env python3
+from math import sin, cos, pi
 
 
-def main():
-    pass
+def f():
+    with open('in.txt', 'r', encoding='utf-8') as f:
+        a = float(f.read())
+    z1 = (((1 + a + a**2) / (2 * a + a**2)) + 2 - ((1 - a + a**2) / (2 * a - a**2))) * (5 - 2 * a**2)
+    z2 = (4 - a**2) / 2
+    with open('out.txt', 'w', encoding='utf-8') as f:
+        f.write(f"z1 = {z1:.4f}\n")
+        f.write(f"z2 = {z2:.4f}\n")
 
 
-main()
+try:
+    f()
+except FileNotFoundError:
+    with open('out.txt', 'w', encoding='utf-8') as f:
+        f.write("Файл со входными данными не найден\n")
+except ZeroDivisionError:
+    with open('out.txt', 'w', encoding='utf-8') as f:
+        f.write("Введено неверное число, произошло деление на 0\n")
+except Exception:
+    with open('out.txt', 'w', encoding='utf-8') as f:
+        f.write("Ожидалось вещественное число")
+
