4230795aad6dcefad48aa5eadb8d3f7584ef46c3
kunansy <kolobov.kirill@list.ru>
2020-11-08 14:44:07 +0300

    Change commit
    Class Routes was documented.
    
     Lab 7, task 1 of programming basics

--- Programming-basics/Labs/Lab7/Task1/task_1.py
+++ Programming-basics/Labs/Lab7/Task1/task_1.py
@@ -73,15 +73,28 @@ class Route:
 
 
 class Routes:
+    """ Class for working with Routes – a list of Routes """
     # for csv writing
     DELIMITER = '\t'
     QUOTECHAR = '"'
     COLUMNS = ["Номер маршрута", "Начало маршрута", "Конец маршрута"]
 
     def __init__(self, routes: List[Route] = None) -> None:
+        """ Init a Routes obj.
+
+        :param routes: list of Route, Route points to work with
+        (empty list by default).
+        :return: None.
+        """
         self._routes = routes or []
 
     def starts_in_point(self, point: str) -> Any:
+        """ Find Routes, which start in the point.
+        Registers are equaled.
+
+        :param point: str, point to find.
+        :return: Routes, routes start in the point.
+        """
         point = point.lower().strip()
         starts = [
             route for route in self._routes
@@ -90,6 +103,12 @@ class Routes:
         return Routes(starts)
 
     def ends_in_point(self, point: str) -> Any:
+        """ Find Routes, which end in the point.
+        Registers are equaled.
+
+        :param point: str, point to find.
+        :return: Routes, routes end in the point.
+        """
         point = point.lower().strip()
         ends = [
             route for route in self._routes
@@ -98,6 +117,11 @@ class Routes:
         return Routes(ends)
 
     def dump(self, path: Path) -> None:
+        """ Dump all Routes into a csv file.
+
+        :param path: Path to csv file to dump the routes.
+        :return: None.
+        """
         rows = [
             [route.num, route.start, route.dest]
             for route in self
@@ -110,20 +134,43 @@ class Routes:
             writer.writerows([self.COLUMNS] + rows)
 
     def add(self, route: Route) -> None:
+        """ Add a route to the list,
+        sort the list by route numbers.
+
+        :param route: Route to add.
+        :return: None.
+        """
         self._routes += [route]
         self._routes.sort(key=lambda route: route.num)
 
     def __iter__(self) -> iter:
+        """ Standard list method.
+
+        :return: iter to the list.
+        """
         return iter(self._routes)
 
     def __bool__(self) -> bool:
+        """ Standard list method.
+
+        :return: whether the list is not empty.
+        """
         return bool(self._routes)
 
     def __iadd__(self, route: Route) -> Any:
+        """ Add a route obj to the list.
+
+        :param route: Route obj to add.
+        :return: self.
+        """
         self.add(route)
         return self
 
     def __str__(self) -> str:
+        """ Convert all routes to a str, divide them.
+
+        :return: str with converted routes.
+        """
         corner = '-' * 20
         inside = '\n' + '-' * 15 + '\n'
 
