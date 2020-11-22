d8bcb3b11964d2082302ae9d02263aab984be93e
kunansy <kolobov.kirill@list.ru>
2020-11-22 16:34:23 +0300

    Completed commit
    Lab4 of programming basics for Eyyub Bodur was completed

--- Programming-basics/Labs-for-collegues/Eyyub-Bodur/Lab4/lab4.py
+++ Programming-basics/Labs-for-collegues/Eyyub-Bodur/Lab4/lab4.py
@@ -12,16 +12,16 @@ def sum_of_positive_nums(arr):
 def max_by_abs(arr):
     mmax = None
     for i in arr:
-        if mmax is None or abs(i) > mmax:
-            mmax = abs(i)
+        if mmax is None or abs(i) > abs(mmax):
+            mmax = i
     return mmax
 
 
 def min_by_abs(arr):
     mmin = None
     for i in arr:
-        if mmin is None or abs(i) < mmin:
-            mmin = abs(i)
+        if mmin is None or abs(i) < abs(mmin):
+            mmin = i
     return mmin
 
 
@@ -32,7 +32,7 @@ except ValueError:
     exit(-1)
 
 arr = []
-for _ in range(n):
+for i in range(n):
     val = round(uniform(-5, 5), 2)
     arr.append(val)
 
@@ -44,7 +44,7 @@ arr.sort(reverse=True)
 mmax = max_by_abs(arr)
 mmin = min_by_abs(arr)
 mult = 1
-print(mmax, mmin)
+
 for i in arr[arr.index(mmax):arr.index(mmin)]:
     mult *= i
 print("Произведение:", mult)
