5d1196f866c4bdb8a24825154e72defb076d942c
kunansy <kolobov.kirill@list.ru>
2020-12-02 08:37:19 +0300

    Completed commit
    Task1 of lab7 of web basics was completed

---
+++ WEB-basics/Labs/Lab7/task1/script.js
@@ -0,0 +1,17 @@
+let num = 10;
+let str = '10';
+let is_winter = true;
+let bsod = null;
+
+// null -> int
+console.log(+bsod)
+// str -> int
+console.log(+str)
+// bool -> int
+console.log(+is_winter)
+// bool -> str
+console.log(`${is_winter}`)
+// int -> str
+console.log(`${num}`)
+// null -> str
+console.log(`${bsod}`)
