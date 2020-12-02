d7372af964852e53b48a4a928e169d9d90f438e0
kunansy <kolobov.kirill@list.ru>
2020-12-02 08:38:25 +0300

    Completed commit
    Task3 of lab7 of web basics was completed

---
+++ WEB-basics/Labs/Lab7/task3/script.js
@@ -0,0 +1,6 @@
+let C = +prompt("Enter graduses in C")
+let F = +prompt("Enter graduses in F")
+
+
+console.log(`${C}\xB0C = ${C * 9/5 + 32}\xB0F`)
+console.log(`${F}\xB0F = ${(F - 32) * 5/9}\xB0C`)
