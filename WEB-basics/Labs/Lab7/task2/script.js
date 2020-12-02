049efff310a9a4db1c25044104c01eb07458aef9
kunansy <kolobov.kirill@list.ru>
2020-12-02 08:37:44 +0300

    Completed commit
    Task2 of lab7 of web basics was completed

---
+++ WEB-basics/Labs/Lab7/task2/script.js
@@ -0,0 +1,10 @@
+let a = +prompt("Enter a");
+let b = +prompt("Enter b");
+
+let square = a * b;
+let perimeter = 2 * (a + b);
+
+console.log(`Square is equal to ${square}`)
+console.log(`Perimeter is equal to ${perimeter}`)
+
+console.log(`Perimeter/square is equal to ${perimeter / square}`)
