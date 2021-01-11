873cd440a3c9a5dbdf51371a625d125482355c38
kunansy <kolobov.kirill@list.ru>
2021-01-11 19:42:21 +0300

    Request updated

--- Programming-basics/Labs/Lab10/request.py
+++ Programming-basics/Labs/Lab10/request.py
@@ -18,7 +18,7 @@ async def get_link(path: Path) -> AsyncIterator[Tuple[str, str]]:
     Asynchronous generator to get link to the site
     and path to where dump its HTML code.
 
-    :param path: Path to the  file.
+    :param path: Path to the file.
     :return: yield tuple of str.
     """
     async with aiofiles.open(path, 'r', encoding='utf-8') as f:
@@ -27,7 +27,8 @@ async def get_link(path: Path) -> AsyncIterator[Tuple[str, str]]:
 
 
 async def dump(content: str,
-               filename: str) -> None:
+               filename: str,
+               **kwargs) -> None:
     """
     Dump page's HTML to to the file.
     All files will be dumped to 'data/' folder.
@@ -36,47 +37,60 @@ async def dump(content: str,
     :param filename: str, filename to where dump the content.
     :return: None.
     """
+    worker_id = kwargs.pop('worker_id', '')
+    worker_id = f"{worker_id}: " * bool(worker_id)
+
     path = DATA_FOLDER / filename
     async with aiofiles.open(path, 'w', encoding='utf-8') as f:
         await f.write(content)
 
-    logger.debug(f"{path} dumped")
+    logger.debug(f"{worker_id}{path} dumped")
 
 
 async def fetch(ses: aiohttp.ClientSession,
-                url: str) -> str or None:
+                url: str,
+                **kwargs) -> str:
     """
     :param ses: aiohttp.ClientSession.
     :param url: str, url from where get HTML code.
-    :return: str, page's HTML code.
+    :return: str, page's HTML code or error text.
     """
+    worker_id = kwargs.pop('worker_id', '')
+    worker_id = f"{worker_id}: " * bool(worker_id)
+
     start = time.time()
-    logger.debug(f"Requested to '{url}'")
+    logger.debug(f"{worker_id}Requested to '{url}'")
     try:
         resp = await ses.get(url)
     except Exception as e:
-        logger.error(f"Error requesting to {url}: {e}")
-        return
+        msg = f"{worker_id}Error requesting to {url}: {e}"
+        logger.error(msg)
+        return msg
 
     if resp.status == 200:
-        logger.debug(f"Received from: '{url}', time={round(time.time() - start, 2)}")
+        executing_time = round(time.time() - start, 2)
+        logger.debug(f"{worker_id}Received from: '{url}', {executing_time=}")
+
         text = await resp.text()
         resp.close()
+
         return text
-    else:
-        logger.error(
-            f"{resp.status} requesting to {resp.url}: {resp.reason}")
-        resp.close()
+
+    msg = f"{worker_id}{resp.status} requesting to {resp.url}: {resp.reason}"
+    logger.error(msg)
+
+    resp.close()
+    return msg
 
 
 async def worker(ses: aiohttp.ClientSession,
-                 queue: asyncio.Queue) -> None:
+                 queue: asyncio.Queue,
+                 worker_id: str) -> None:
     while True:
         url, filename = queue.get_nowait()
 
-        text = await fetch(ses, url)
-        if text:
-            await dump(text, filename)
+        text = await fetch(ses, url, worker_id=worker_id)
+        # await dump(text, filename, worker_id=worker_id)
 
         queue.task_done()
 
@@ -99,8 +113,10 @@ async def bound_fetch(path: Path) -> None:
             queue.put_nowait((url, filename))
 
         tasks = []
-        for _ in range(5):
-            task = asyncio.create_task(worker(ses, queue))
+        for worker_id in range(5):
+            worker_id = f"Worker-{worker_id + 1}"
+            task = asyncio.create_task(
+                worker(ses, queue, worker_id=worker_id))
             tasks += [task]
 
         await queue.join()
@@ -113,4 +129,3 @@ def main(link_path: Path) -> None:
     start = time.time()
     asyncio.run(bound_fetch(link_path))
     logger.info(f"Working time: {time.time() - start:.2f}")
-
