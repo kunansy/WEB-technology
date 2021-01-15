9b503eb1609015ed67dc04c6dec30abfd3860081
Kirill <kolobov.kirill@list.ru>
2021-01-15 15:14:03 +0300

    Python project (#1)
    * Initial commit
    
    Project of programming basics was started
    
    * Initial commit
    
    File with links was added to programming basics project
    
    * Initial commit
    
    Requirements were added to programming basics project
    
    * Change commit
    
    Request module of programming basics project is in process
    
    * Add commit
    
    File with 100 links was added to programming basics project
    
    * Add commit
    
    File with 10 links was added to programming basics project
    
    * Change commit
    
    File with 10 links of programming basics project was renamed
    
    * Change commit
    
    File with 100 links of programming basics project was renamed
    
    * Delete commit
    
    File with pure links without filenames was removed. Programming basics
    project
    
    * Change commit
    
    Python project of programming basics, requirements were updated:
    aiojobs was added, colorama was removed
    
    * Change commit
    
    Python project of programming basics, request module was changed a log
    
    * Change commit
    
    Python project of programming basics, request module was changed
    
    * initial commit
    
    Python project of programming basics, gui file was started
    
    * Initial commit
    
    Python project of programming basics, main file was started
    
    * Add commit
    
    Python project of programming basics, logging was added
    
    * Change commit
    
    Pyrhon project of programming basics, logging and docs were added
    
    * Change commit
    
    Minor fixes and improvements
    
    * Links lists corrected
    
    Some wring addresses removed, .html added to filenames
    
    * Worker added
    
    * Minor fixes
    
    * Minor fixes
    
    * Creating logger changed
    
    * Working with handlers/logger level added
    
    * Minor changes
    
    * Gitignore added
    
    * Some slow links removed
    
    * Request updated
    
    * Slow site removed
    
    * Working with logger changed
    
    * Request updated
    
    * Way to get env var changed
    
    * aiojobs removed from requirements
    
    * Packages versions specified
    
    * nc
    
    * list with 100 links fixed
    
    * list with 10 links fixed
    
    * Validating file lines added
    
    * Requirements updated
    
    PyQt added
    
    * CLI args parsing added
    
    * File mode of the main file changed
    
    Now it's executable
    
    * Way to work with 'data_folder' changed
    
    Now it explicetly passed as an argument, but not env var
    
    * UI file added
    
    * Request module completed
    
    * Main module completed
    
    * GUI module completed
    
    * Imports optimized
    
    * Minor cosmetic change
    
    * Logs browser extended
    
    * Default file handler log level changed

---
+++ Programming-basics/Labs/Lab10/main.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import os
+from pathlib import Path
+from typing import Union
+
+import gui
+import request
+
+LEVEL = Union[int, str]
+
+MSG_FMT = "[{asctime},{msecs:3.0f}] [{levelname}] " \
+          "[{module}:{funcName}] {message}"
+DATE_FMT = "%d.%m.%Y %H:%M:%S"
+
+LOG_FOLDER = Path('logs')
+os.makedirs(LOG_FOLDER, exist_ok=True)
+
+formatter = logging.Formatter(
+    fmt=MSG_FMT, datefmt=DATE_FMT, style='{'
+)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(formatter)
+
+path = LOG_FOLDER / f"lab10.log"
+file_handler = logging.FileHandler(
+    path, delay=True, encoding='utf-8')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger = logging.getLogger("web-scraper")
+logger.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+
+def set_handler_level(handler_class: type):
+    def wrapped(level: LEVEL) -> None:
+        try:
+            level = level.upper()
+        except AttributeError:
+            pass
+
+        for handler in logger.handlers:
+            if isinstance(handler, handler_class):
+                handler.setLevel(level)
+                return
+        logger.error(f"There is no '{handler_class}' handler.")
+
+    return wrapped
+
+
+set_stream_handler_level = set_handler_level(logging.StreamHandler)
+set_file_handler_level = set_handler_level(logging.FileHandler)
+
+
+def set_logger_level(level: LEVEL) -> None:
+    try:
+        level = level.upper()
+    except AttributeError:
+        pass
+    logger.setLevel(level)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Get HTML codes of the sites"
+    )
+    parser.add_argument(
+        '-f', '--file',
+        type=str,
+        help="File where there are links and expected file names",
+        dest='links'
+    )
+    parser.add_argument(
+        '-l', '--link',
+        type=str,
+        nargs=2,
+        help="Link to get its HTML code and file name from where put it",
+        metavar="LINK",
+        dest='links'
+    )
+    parser.add_argument(
+        '-dest',
+        type=str,
+        help="Folder to where save HTML codes",
+        required=False,
+        default=os.getenv('DATA_FOLDER', 'data')
+    )
+    parser.add_argument(
+        '--log-level',
+        type=str,
+        help="Set stream handler level",
+        choices=['debug', 'info', 'warning', 'error', 'critical'],
+        default='debug',
+        dest='level'
+    )
+    parser.add_argument(
+        "--no-gui",
+        help="Whether start app without GUI",
+        action="store_true",
+        default=False,
+        dest="no_gui"
+    )
+    args = parser.parse_args()
+
+    os.environ['DATA_FOLDER'] = args.dest
+    os.makedirs(args.dest, exist_ok=True)
+
+    set_stream_handler_level(args.level)
+
+    if args.no_gui is False:
+        gui.main()
+        return
+
+    if args.links is None:
+        raise ValueError("Links not set")
+
+    links = args.links
+    from_link = False
+    if isinstance(links, list):
+        links = ' '.join(links)
+        from_link = True
+    else:
+        links = Path(args.links)
+
+    request.main(links, Path(args.dest), from_link)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Programming-basics/Labs/Lab10/request.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+import asyncio
+import logging
+import os
+import re
+import time
+from pathlib import Path
+from typing import AsyncIterator, Tuple, AsyncGenerator
+
+import aiofiles
+import aiohttp
+
+logger = logging.getLogger('web-scraper')
+IS_LINE_CORRECT = re.compile(r'http(s)?://.+ .+', re.IGNORECASE)
+
+
+async def from_link(line: str) -> AsyncIterator[Tuple[str, str]]:
+    yield line.split()
+
+
+async def from_file(path: Path) -> AsyncIterator[Tuple[str, str]]:
+    """
+    Asynchronous generator to get link to the site
+    and path to where dump its HTML code.
+
+    :param path: Path to the file.
+    :return: yield tuple of str.
+    """
+    async with aiofiles.open(path, 'r', encoding='utf-8') as f:
+        async for link in f:
+            if IS_LINE_CORRECT.search(link):
+                yield link.split()
+            else:
+                logger.error(
+                    f"Line must be like 'http(s)://link filename', "
+                    f"but '{link}' found")
+
+
+async def dump(content: str,
+               filename: str,
+               **kwargs) -> None:
+    """
+    Dump page's HTML to to the file.
+    All files will be dumped to 'data/' folder.
+
+    :param content: str, HTML code to dump.
+    :param filename: str, filename to where dump the content.
+    :keyword worker_id: str, way to identify the worker.
+    :keyword data_folder: Path to folder to where save the files.
+
+    :return: None.
+    """
+    worker_id = kwargs.pop('worker_id', '')
+    worker_id = f"{worker_id}: " * bool(worker_id)
+
+    path = kwargs.pop('data_folder', Path('.')) / filename
+    async with aiofiles.open(path, 'w', encoding='utf-8') as f:
+        await f.write(content)
+
+    logger.debug(f"{worker_id}{path} dumped")
+
+
+async def fetch(ses: aiohttp.ClientSession,
+                url: str,
+                **kwargs) -> str:
+    """
+    Get HTML code of the page.
+
+    :param ses: aiohttp.ClientSession.
+    :param url: str, url from where get HTML code.
+    :keyword worker_id: str, way to identify the worker.
+
+    :return: str, page's HTML code or error text.
+    """
+    worker_id = kwargs.pop('worker_id', '')
+    worker_id = f"{worker_id}: " * bool(worker_id)
+
+    start = time.time()
+    logger.debug(f"{worker_id}Requested to '{url}'")
+
+    try:
+        resp = await ses.get(url)
+    except Exception as e:
+        msg = f"{worker_id}Error requesting to {url}: {e}"
+        logger.error(msg)
+        return msg
+
+    if resp.status == 200:
+        executing_time = round(time.time() - start, 2)
+        logger.debug(f"{worker_id}Received from: '{url}', {executing_time=}")
+
+        text = await resp.text()
+        resp.close()
+
+        return text
+
+    msg = f"{worker_id}{resp.status} requesting to {resp.url}: {resp.reason}"
+    logger.error(msg)
+
+    resp.close()
+    return msg
+
+
+async def worker(ses: aiohttp.ClientSession,
+                 queue: asyncio.Queue,
+                 *,
+                 worker_id: str,
+                 data_folder: Path) -> None:
+    """
+    Worker fetching HTML code of the site and dumping it to file.
+
+    :param ses: aiohttp.ClientSession.
+    :param queue: asyncio.Queue with args.
+    :param worker_id: str, way to identify the worker.
+    :param data_folder: Path to folder to where save the files.
+
+    :return: None.
+    """
+    while True:
+        url, filename = queue.get_nowait()
+
+        text = await fetch(ses, url, worker_id=worker_id)
+        await dump(
+            text, filename, worker_id=worker_id, data_folder=data_folder)
+
+        queue.task_done()
+
+
+async def bound_fetch(data_folder: Path,
+                      links: AsyncGenerator) -> None:
+    """
+    Run coro, get HTML codes and dump
+    them to files using 5 workers for it.
+
+    There is timeout = 5s.
+
+    :param data_folder: Path to folder to
+     where save the .html files.
+    :param links: async generator from where
+     get the links and file names.
+
+    :return: None.
+    """
+    timeout = aiohttp.ClientTimeout(5)
+    queue = asyncio.Queue()
+
+    async with aiohttp.ClientSession(timeout=timeout) as ses:
+        async for url, filename in links:
+            queue.put_nowait((url, filename))
+
+        tasks = []
+        for worker_id in range(5):
+            worker_id = f"Worker-{worker_id + 1}"
+            task = asyncio.create_task(
+                worker(ses, queue,
+                       worker_id=worker_id, data_folder=data_folder)
+            )
+            tasks += [task]
+
+        await queue.join()
+
+        for task in tasks:
+            task.cancel()
+
+
+def main(links: str or Path,
+         data_folder: Path or str = os.getenv('DATA_FOLDER', 'data'),
+         from_line: bool = False) -> None:
+    data_folder = Path(data_folder)
+
+    logger.info("Requesting started")
+    start = time.time()
+    if from_line:
+        if IS_LINE_CORRECT.search(links) is None:
+            msg = f"Line must be like 'http(s)://link filename', " \
+                  f"but '{links}' found"
+            logger.error(msg)
+            raise ValueError(msg)
+
+        # noinspection PyTypeChecker
+        asyncio.run(bound_fetch(data_folder, from_link(links)))
+    else:
+        if not links.exists():
+            msg = f"File with links '{links}' not found"
+            logger.error(msg)
+            raise FileNotFoundError(msg)
+
+        # noinspection PyTypeChecker
+        asyncio.run(bound_fetch(data_folder, from_file(links)))
+
+    logger.info(f"Working time: {time.time() - start:.2f}")
